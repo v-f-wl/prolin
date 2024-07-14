3b243ee7449d0ddb272702e01982c19f83231079
Kim Valentin <co.unicorno@mail.ru>
2024-07-14 22:32:01 +0400

    feat: add modal component

--- app/lobby-session/[lobbyId]/@admin/page.tsx
+++ app/lobby-session/[lobbyId]/@admin/page.tsx
@@ -1,4 +1,5 @@
 import MainContainer from "@/app/_components/admin-lobby/main-side";
+import ModalCreate from "@/app/_components/admin-lobby/modal-create-game";
 import SideBar from "@/app/_components/admin-lobby/sidebar";
 
 const AdminPage = () => {
@@ -6,6 +7,7 @@ const AdminPage = () => {
     <div className="md:container h-full md:mx-auto grid grid-cols-main-lobby gap-9 py-8">
       <SideBar/>
       <MainContainer/>
+      <ModalCreate/>
     </div>
    );
 }
